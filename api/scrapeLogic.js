68498b0cee872ae895264061d39fb9a9221d018d
alproviste <alaintai.2000@hotmail.com>
2023-07-17 13:08:50 +0200

    update --> Headless
    update --> Headless

--- api/scrapeLogic.js
+++ api/scrapeLogic.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 const scrapeLogic = async (req, res) => {
   const browser = await puppeteer.launch({
-    headless: false,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
