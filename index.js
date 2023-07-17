3d6fbc9aed5728cb7fa734f4e601cf4540764b01
alproviste <alaintai.2000@hotmail.com>
2023-07-17 15:58:44 +0200

    update --> setup done
    update --> setup done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fetch = require('node-fetch')
 const express = require("express");
 const { scrapeLogic } = require("./api/scrapeLogic");
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
 
@@ -17,4 +20,45 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/openapi.yaml', (req, res) => {
+  const host = req.headers.host;
+  fs.readFile('openapi.yaml', 'utf8', (err, data) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('An error occurred while reading the file.');
+          return;
+      }
+      const text = data.replace('PLUGIN_HOSTNAME', `https://${host}`);
+      res.set('Content-Type', 'text/yaml');
+      res.send(text);
+  });
+});
+
+
+app.get('/.well-known/ai-plugin.json', (req, res) => {
+  const host = req.headers.host;
+  const filePath = path.join(__dirname, '.well-known', 'ai-plugin.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('An error occurred while reading the file.');
+          return;
+      }
+      const text = data.replace('PLUGIN_HOSTNAME', `http://${host}`);
+      res.set('Content-Type', 'application/json');
+      res.send(text);
+  });
+});
+
+
+app.get('/OneForOneLogo.svg', (req, res) => {
+  const filename = path.join(__dirname, 'OneForOneLogo.svg');
+  res.sendFile(filename, (err) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('An error occurred while sending the file.');
+      }
+  });
 });
